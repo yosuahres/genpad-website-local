935ce5094d5d00e70cc38d3a8c0e964f31234fa2
yosuahres <haresyosuaa@gmail.com>
2026-01-24 21:24:18 +0700

    feat: setup shared workspace configs and types package

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
     origin: 'http://localhost:8080',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
-  })
+  });
 
   await app.listen(process.env.PORT ?? 3000);
 }
