b51bbdeca6abda80cd333a67e80cd96850a3521a
yosuahres <haresyosuaa@gmail.com>
2026-01-24 20:43:58 +0700

    feat: add github action workflow & setup cors

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //setup cors
+  app.enableCors({
+    origin: 'http://localhost:8080',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  })
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
